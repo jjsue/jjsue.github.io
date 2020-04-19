358e6968a31dd06854b4db5b780a3df234f19499
jjsue <jjsue_95@hotmail.es>
2020-04-19 14:09:04 +0200

    Deploy

---
+++ precache-manifest.bdd299fa440f7d79a4d8dc05d487f155.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79dc78525a41036da25cb84991b7405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b90cca5c766e0d2afa",
+    "url": "/static/css/2.eff14830.chunk.css"
+  },
+  {
+    "revision": "84b90cca5c766e0d2afa",
+    "url": "/static/js/2.631bd146.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.631bd146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fd50911da877441897",
+    "url": "/static/js/main.696073d1.chunk.js"
+  },
+  {
+    "revision": "e7ed4d20c3d0773f98e7",
+    "url": "/static/js/runtime-main.d70c4a7a.js"
+  },
+  {
+    "revision": "10d2041eeeea705f10cdbbfc26eb2096",
+    "url": "/static/media/startBell.10d2041e.wav"
+  }
+]);
